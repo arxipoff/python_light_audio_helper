3841e975c96f1f9e11629ca131432b5acbec325b
arxip@inbox.ru <Poiskeone03>
2019-02-22 12:01:11 +0300

    add some command for job joke

--- index.py
+++ index.py
@@ -3,11 +3,10 @@ import os
 import sys
 import webbrowser
 
+
 def talk (words):
     print(words)
     os.system('say ' + words)
-
-talk('Привет кожанный ублюдок, чё тебе надо')
 
 
 def command():
@@ -30,16 +29,30 @@ def command():
 
 
 def makeSome(task):
-    if 'открыть сайт' in task:
+    if 'проснись и пой' in task:
+        talk('Привет кожанный ублюдок, чё тебе надо')
+
+
+    elif 'открой google' in task:
         talk('Откываю жди какашка')
         url = 'https://google.com'
         webbrowser.open(url)
+    elif 'открой яндекс' in task:
+        talk('Откываю жди какашка')
+        url = 'https://yandex.ru'
+        webbrowser.open(url)    
+
+
     elif 'кто такая ира' in task:
-        talk('Ты что дурак? это ваш главнй женский человек')
+        talk('Ты что дурак? это ваш главный женский человек')
     elif 'кто такая наташа' in task:
         talk('Наташка заведует вашей работой. С ней не шути кожанный дурачёк')
     elif 'кто такой серёжа' in task:
         talk('Ой зайбей вообще, чувак пишет на питоне. Прости серёга')
+    elif 'кто такой виталик' in task:
+        talk('Это наш бро. За бротана и двор стреляю в упор')
+    elif 'кто такой ваня' in task:
+        talk('Ваня монстр, он и ios и android')
     elif 'убейся' in task:
         talk('Удаляюсь, кричи если что глупый организм')
         sys.exit()
